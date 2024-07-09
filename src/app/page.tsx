6664df9b250f8e0f36ffc3c7b80baf2122b297fa
riyalohia <riyalh1997@gmail.com>
2024-07-09 08:45:55 +0530

    Adds changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,104 @@
-import Image from "next/image";
+"use client"
+import { useState } from "react";
+import { tasks as initialTasks } from "./mocks";
 
 export default function Home() {
+  const [query, setQuery] = useState('')
+  const [tasks, setTasks] = useState(initialTasks)
+  const [showAddTaskDialog, setAddTaskDialog] = useState(false)
+  const [newTask, setNewTask] = useState({})
+
+  const onSearchChange = (e) => {
+    const query = e.target.value
+    setQuery(query)
+  }
+
+  const onSearch = () => {
+    const updatedTasks = initialTasks.filter(task => task.category.toLowerCase().includes(query.toLowerCase()))
+    setTasks(updatedTasks)
+  }
+
+  const oncancel = () => {
+    setQuery('')
+    setTasks(initialTasks)
+  }
+
+  const addTask = () => {
+    setAddTaskDialog(true)
+  }
+
+  const createNewTask = (value: string, type: string) => {
+    const updatedNewTask = { ...newTask, [type]: value }
+    setNewTask(updatedNewTask)
+  }
+
+  const addNewtask = () => {
+    const id = tasks.length + 1
+    const updatedTasks = [...tasks]
+    updatedTasks.push({
+      ...newTask,
+      id,
+      status: 'pending'
+    })
+    setTasks(updatedTasks)
+  }
+
+  const changeStatus = (id: number) => {
+    const updatedTask = tasks.filter(task => task.id === id)
+    const updatedTaskStatus = {
+      ...updatedTask,
+      status: 'completed'
+    }
+    const tasksListCopy = [...tasks]
+    tasksListCopy[id-1] = updatedTaskStatus
+    setTasks(tasksListCopy)
+  }
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <main className="">
+      <div className="flex gap-3">
+        <button onClick={addTask}>Add task</button>
+        <input
+          value={query}
+          onChange={onSearchChange}
         />
+        <button onClick={onSearch}>Search</button>
+        <button onClick={oncancel}>Cancel</button>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div>
+        {tasks.map(task => (
+          <div key={task.id} className="flex gap-5">
+            <div>
+              <p>{task.title}</p>
+              <p>{task.subtitle}</p>
+            </div>
+            <button onClick={() => changeStatus(task.id)}>Done</button>
+            <button>Delete</button>
+          </div>
+        ))}
       </div>
+      {showAddTaskDialog && (
+        <div>
+          Add new task
+          <div className="flex gap-2">
+            <p>Title</p>
+            <input onChange={(e) => createNewTask(e.target.value, 'title')} />
+          </div>
+          <div className="flex gap-2">
+            <p>Subtitle</p>
+            <input onChange={(e) => createNewTask(e.target.value, 'subtitle')} />
+          </div>
+          <div className="flex gap-2">
+            <p>Category</p>
+            <input onChange={(e) => createNewTask(e.target.value, 'category')} />
+          </div>
+          <div className="flex gap-2">
+            <button onClick={addNewtask}>Add</button>
+            <button>Cancel</button>
+          </div>
+        </div>
+      )}
     </main>
   );
 }
